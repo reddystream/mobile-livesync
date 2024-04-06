42781bf1de36c972ec58552964d5d747ec7bb8bf
Webster Alan Ferreira Sousa <metasquare.co@gmail.com>
2024-04-06 16:42:41 -0300

    transição de activities e definição de SplashActivity como launcher principal

---
+++ app/src/main/java/com/app/unihub/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.app.unihub;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.app.unihub.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.splash);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+
+}
