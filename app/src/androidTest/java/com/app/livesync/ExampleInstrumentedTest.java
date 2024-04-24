13891e4eada092253c50fc120b0adb64e89248d9
Webster Alan Ferreira Sousa <metasquare.co@gmail.com>
2024-04-24 13:52:04 -0300

    ExampleInstrumentedTest - refactoring name of project

--- app/src/androidTest/java/com/app/livesync/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/app/livesync/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.app.unihub", appContext.getPackageName());
+        assertEquals("com.app.livesync", appContext.getPackageName());
     }
 }
